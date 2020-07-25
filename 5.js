8b4efb2512cd355416ce68512a6dfa86d864bdc7
nabilaafifahqotrunnada <nabila_afifah_28rpl@student.smktelkom-mlg.sch.id>
2020-07-25 11:57:12 +0700

    Second Commit

---
+++ 5.js
@@ -0,0 +1,22 @@
+//array kota
+let kota = ["Malang", "Surabaya", "Tulungagung"]
+
+//keterangan
+console.log("Isi array kota");
+//menampilkan array kota
+console.log(kota);
+//menampilkan jumlah data pada array kota
+console.log("Jumlah data kota = " + kota.length);
+
+//mencetak pembatas data
+console.log("-------------------------------------");
+
+//tambah data kota baru pada array kota
+kota.push("Banyuwangi")
+
+//keterangan baru
+console.log("Isi array kota saat ini");
+//menampilkan array kota setelah diperbarui
+console.log(kota);
+//menampilkan jumlah data array kota yang baru
+console.log("Jumlah data kota saat ini = " + kota.length);
